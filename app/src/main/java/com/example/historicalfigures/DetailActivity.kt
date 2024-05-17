9135b389c94be22cdebd131fbc484cca45687019
Yernur Shapkat <113994667+Fruzzy07@users.noreply.github.com>
2024-05-17 22:16:56 +0500

    Initial commit

--- app/src/main/java/com/example/historicalfigures/DetailActivity.kt
+++ app/src/main/java/com/example/historicalfigures/DetailActivity.kt
@@ -3,16 +3,36 @@ package com.example.historicalfigures
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
+import android.content.Context
 
 class DetailActivity : AppCompatActivity() {
+
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
+    private lateinit var editTextNotes: EditText
+
+    // В DetailActivity.kt
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
+        // Получаем индекс месяца
         val monthIndex = intent.getIntExtra("MONTH_INDEX", -1)
+
+        sharedPreferences = getSharedPreferences("Notes", Context.MODE_PRIVATE)
+        editor = sharedPreferences.edit()
+
+        editTextNotes = findViewById(R.id.edittext_notes)
+
+        // Получаем сохраненные заметки для текущего месяца
+        val savedNotes = sharedPreferences.getString("notes_$monthIndex", "")
+        editTextNotes.setText(savedNotes)
 
         val textViewDetail = findViewById<TextView>(R.id.textview_detail)
         textViewDetail.text = "Detail for month: $monthIndex"
@@ -28,15 +48,22 @@ class DetailActivity : AppCompatActivity() {
         }
     }
 
+    private fun saveNotes(monthIndex: Int) {
+        val notes = editTextNotes.text.toString()
+        editor.putString("notes_$monthIndex", notes) // Сохраняем заметки с использованием ключа, основанного на индексе месяца
+        editor.apply()
+    }
+
     private fun setResultAndFinish(resultCode: Int, monthIndex: Int, status: String) {
+        saveNotes(monthIndex) // Сохраняем заметки перед завершением активности
         val resultIntent = Intent().apply {
             putExtra("MONTH_INDEX", monthIndex)
             putExtra("STATUS", status)
         }
         setResult(resultCode, resultIntent)
-        finish()
     }
 }
+
 
 
 
--- app/src/main/java/com/example/historicalfigures/OtherActivity.kt
+++ app/src/main/java/com/example/historicalfigures/OtherActivity.kt
@@ -3,6 +3,7 @@ package com.example.historicalfigures
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
@@ -105,6 +106,7 @@ class OtherActivity : AppCompatActivity() {
                                     0,
                                     1f
                                 )
+                                yearTextView.id = View.generateViewId()
                                 yearsContainer.addView(yearTextView)}
                         } else {
                             Toast.makeText(this@OtherActivity, "Country not found or data unavailable", Toast.LENGTH_SHORT).show()
